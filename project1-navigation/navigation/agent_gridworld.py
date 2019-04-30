c10c00261aaffb5b4b0dcb49655ce1741b019ca5
wpumacay <wpumacay@gmail.com>
2019-04-30 11:37:55 -0500

    [ADD]: test case for gridworld

--- project1-navigation/navigation/agent_gridworld.py
+++ project1-navigation/navigation/agent_gridworld.py
@@ -6,48 +6,43 @@ import numpy as np
 from navigation.dqn.core import agent
 from navigation.dqn.utils import config 
 
-class DqnBananaRaycastAgent( agent.IDqnAgent ) :
+class DqnGridworldAgent( agent.IDqnAgent ) :
 
     def __init__( self, agentConfig, modelConfig, modelBuilder, backendInitializer ) :
-        super( DqnBananaRaycastAgent, self ).__init__( agentConfig, modelConfig, modelBuilder, backendInitializer )
+        super( DqnGridworldAgent, self ).__init__( agentConfig, modelConfig, modelBuilder, backendInitializer )
 
     def _preprocess( self, rawState ) :
-        """Default preprocessing by just copying the data
+        # rawState is an index, so convert it to a one-hot representation
+        _stateOneHot = np.zeros( self._stateDim )
+        _stateOneHot[rawState] = 1.0
 
-        Args:
-            rawState (np.ndarray) : raw state from lunar lander environment
-
-        Returns:
-            np.ndarray : copy of the gym-env. observation for the model
-
-        """
-        return rawState.copy()
+        return _stateOneHot
 
 AGENT_CONFIG = config.DqnAgentConfig()
-AGENT_CONFIG.stateDim                   = 37
-AGENT_CONFIG.nActions                   = 4
+AGENT_CONFIG.stateDim                   = -1 # filled by trainer
+AGENT_CONFIG.nActions                   = -1 # filled by trainer
 AGENT_CONFIG.epsilonSchedule            = 'geometric'
 AGENT_CONFIG.epsilonStart               = 1.0
 AGENT_CONFIG.epsilonEnd                 = 0.1
 AGENT_CONFIG.epsilonDecay               = 0.995
 AGENT_CONFIG.lr                         = 0.001
-AGENT_CONFIG.minibatchSize              = 64
+AGENT_CONFIG.minibatchSize              = 32
 AGENT_CONFIG.learningStartsAt           = 0
-AGENT_CONFIG.learningUpdateFreq         = 10
-AGENT_CONFIG.learningUpdateTargetFreq   = 10
-AGENT_CONFIG.learningMaxSteps           = 2000
-AGENT_CONFIG.replayBufferSize           = 20000
+AGENT_CONFIG.learningUpdateFreq         = 4
+AGENT_CONFIG.learningUpdateTargetFreq   = 4
+AGENT_CONFIG.learningMaxSteps           = 1000
+AGENT_CONFIG.replayBufferSize           = 10000
 AGENT_CONFIG.discount                   = 0.999
 AGENT_CONFIG.tau                        = 0.001
 AGENT_CONFIG.seed                       = 0
 
 MODEL_CONFIG = config.DqnModelConfig()
-MODEL_CONFIG.inputShape = (37,)
-MODEL_CONFIG.outputShape = (4,)
+MODEL_CONFIG.inputShape     = (-1,) # filled by trainer
+MODEL_CONFIG.outputShape    = (-1,) # filled by trainer
 MODEL_CONFIG.layers = [ { 'name': 'fc1', 'type' : 'fc', 'units' : 128, 'activation' : 'relu' },
                         { 'name': 'fc2', 'type' : 'fc', 'units' : 64, 'activation' : 'relu' },
                         { 'name': 'fc3', 'type' : 'fc', 'units' : 16, 'activation' : 'relu' },
                         { 'name': 'fc4', 'type' : 'fc', 'units' : -1, 'activation' : 'linear' } ]
 
 def CreateAgent( agentConfig, modelConfig, modelBuilder, backendInitializer ) :
-    return DqnBananaRaycastAgent( agentConfig, modelConfig, modelBuilder, backendInitializer )
+    return DqnGridworldAgent( agentConfig, modelConfig, modelBuilder, backendInitializer )
--- project1-navigation/trainer.py
+++ project1-navigation/trainer.py
@@ -3,26 +3,49 @@ import os
 import numpy as np
 from tqdm import tqdm
 from collections import deque
+from collections import defaultdict
 
 # import simple gridworld for testing purposes
 from navigation.envs import mlagents
 
+# @DEBUG: test environment - gridworld (sanity check) --------------------------
+from rl.envs import gridworld
+from rl.envs import gridworld_utils
+import matplotlib.pyplot as plt
+# ------------------------------------------------------------------------------
+
 # import banana agent (raycast )
 from navigation import agent_raycast
+# @DEBUG: gridworl agent for testing
+from navigation import agent_gridworld
 
 # import model builder functionality (pytorch as backend)
 from navigation import model_pytorch
 
 from IPython.core.debugger import set_trace
 
+GRIDWORLD = True
 TEST = False
 
+# @DEBUG: test method for gridworld --------------------------------------------
+def plotQTable( envGridworld, agentGridworld ) :
+    plt.ion()
+    # evaluate the agents model for each action state
+    _qvals_sa = defaultdict( lambda : np.zeros( envGridworld.nA ) )
+    for s in range( envGridworld.nS ) :
+        if envGridworld._isTerminal( s ) :
+            _qvals_sa[s] = np.zeros( envGridworld.nA )
+        else :
+            _qvals_sa[s] = agentGridworld._qmodel_actor.eval( agentGridworld._preprocess( s ) )
 
+    gridworld_utils.plotQTableInGrid( _qvals_sa, envGridworld.rows, envGridworld.cols )
+
+# ------------------------------------------------------------------------------
 
 def train( env, agent, savefile ) :
     MAX_EPISODES = agent.learningMaxSteps
     MAX_STEPS_EPISODE = 1000
-    LOG_WINDOW_SIZE = 1
+    LOG_WINDOW_SIZE = 100
 
     _progressbar = tqdm( range( 1, MAX_EPISODES + 1 ), desc = 'Training>', leave = True )
     _maxAvgScore = -np.inf
@@ -32,7 +55,7 @@ def train( env, agent, savefile ) :
 
     for iepisode in _progressbar :
 
-        _state = env.reset( training = True )
+        _state = env.reset()
         _score = 0
         _nsteps = 0
 
@@ -68,11 +91,17 @@ def train( env, agent, savefile ) :
             # log results
             if iepisode % LOG_WINDOW_SIZE == 0 :
                 ## set_trace()
-                _progressbar.set_description( 'Training> Max-Avg=%.2f, Curr-Avg=%.2f, Curr=%.2f, Eps=%.2f' % (_maxAvgScore, _avgScore, _score, agent.epsilon ) )
+                _progressbar.set_description( 'Training> Max-Avg=%.2f, Curr-Avg=%.2f, Curr=%.2f, Eps=%.2f, Nsteps=%d' % (_maxAvgScore, _avgScore, _score, agent.epsilon, _nsteps ) )
                 _progressbar.refresh()
 
-    if savefile is not None :
-        agent.save( savefile )
+    if GRIDWORLD :
+        # @DEBUG: gridworl visualization of q-table---------------------------------
+        plotQTable( env, agent )
+        _ = input( 'Press ENTER to continue ...' )
+        # --------------------------------------------------------------------------
+    else :
+        if savefile is not None :
+            agent.save( savefile )
 
 def test( env, agent ) :
     for _ in range( 10 ) :
@@ -91,12 +120,37 @@ def experiment() :
     _bananaExecPath = os.path.join( os.getcwd(), 'executables/Banana_Linux/Banana.x86_64' )
     _bananaHeadlessExecPath = os.path.join( os.getcwd(), 'executables/Banana_Linux_NoVis/Banana.x86_64' )
 
-    # instantiate the environment
-    _env = mlagents.createDiscreteActionsEnv( _bananaExecPath if TEST else _bananaHeadlessExecPath )
+    if not GRIDWORLD :
+        # instantiate the environment
+        _env = mlagents.createDiscreteActionsEnv( _bananaExecPath )
 
-    # instantiate the agent
-    _agent = agent_raycast.CreateAgent( model_pytorch.DqnModelBuilder,
-                                        model_pytorch.BackendInitializer )
+        # instantiate the agent
+        _agent = agent_raycast.CreateAgent( agent_raycast.AGENT_CONFIG,
+                                            agent_raycast.MODEL_CONFIG,
+                                            model_pytorch.DqnModelBuilder,
+                                            model_pytorch.BackendInitializer )
+    else :
+        # @DEBUG: gridworld test environment------------------------------------
+        _env = gridworld.GridWorldEnv( gridworld.BOOK_LAYOUT, # DEFAULT_LAYOUT
+                                       noise = 0.0,
+                                       rewardAtGoal = -1.0, # 10.0
+                                       rewardAtHole = -1.0, # -10.0
+                                       rewardPerStep = -1.0,
+                                       renderInteractive = TEST,
+                                       randomSeed = 0 )
+
+        agent_gridworld.AGENT_CONFIG.stateDim = _env.nS
+        agent_gridworld.AGENT_CONFIG.nActions = _env.nA
+    
+        agent_gridworld.MODEL_CONFIG.inputShape   = ( _env.nS, )
+        agent_gridworld.MODEL_CONFIG.outputShape  = ( _env.nA, )
+
+        _agent = agent_gridworld.CreateAgent( agent_gridworld.AGENT_CONFIG,
+                                              agent_gridworld.MODEL_CONFIG,
+                                              model_pytorch.DqnModelBuilder,
+                                              model_pytorch.BackendInitializer )
+
+        # ----------------------------------------------------------------------
 
     if not TEST :
         train( _env, _agent, 'banana_model_weights.pth' )
